bb3c1e372a1cc04d495d6e4a6ff5102f3961ea08
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-02-19 10:18:45 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.baseText}>This is Watcher App Template</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  baseText: {
+    color: "#000",
+  }
+});
