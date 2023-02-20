2895189e5a842b2d14ff078dd070541e186bf631
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-02-20 13:56:30 +0530

    UI Improved And Added Add Goals Functionality

--- App.tsx
+++ App.tsx
@@ -1,26 +1,66 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState<string | null>(null);
+  const [courseGoals, setCourseGoals] = useState<Array<string | null>>([]);
+
+  const goalInputHandler = (enteredText:string) =>{
+
+    setEnteredGoalText(enteredText);
+
+  };
+
+  const addGoalHandler = () =>{
+
+      setCourseGoals((currentCourseGoals) => [...currentCourseGoals,enteredGoalText]);
+
+  };
+
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.baseText}>This is Watcher App Template</Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+      <SafeAreaView style={styles.appContainer}>
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Your Course Goal" onChangeText={goalInputHandler}/>
+          <Button title="Add Goal" onPress={addGoalHandler}/>
+        </View>
+        
+        <View style={styles.goalsContainer}>
+          {courseGoals.map((goal,index) => <Text key={index}>{goal}</Text>)}
+        </View>
+      </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
 
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  baseText: {
-    color: "#000",
-  }
+    appContainer : {
+      padding : 50,
+      paddingHorizontal : 28,
+      flex : 1,
+      flexDirection : "column",
+      backgroundColor : "#000000", 
+    },
+    inputContainer:{
+      flex : 1,
+      flexDirection : "row",
+      justifyContent : "space-between",
+      alignItems : "center",
+      marginBottom : 24,
+      borderBottomWidth : 1,
+      borderBottomColor : "#cccccc",
+    },
+    textInput : {
+        borderWidth : 1,
+        borderColor : "#cccccc",
+        width : "70%",
+        marginRight : 8,
+        padding : 8,
+    },
+    goalsContainer : {
+      flex : 5,
+    },
 });
